ce8146f41df89d074edcbe533d0a6f14928d5f7a
Aj8917 <ajinkyaabhosale@gmail.com>
2025-07-21 12:27:21 +0530

    Replaced subscribly-client with new frontend

---
+++ subscribly-client/src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+import Navbar from './components/includes/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar />
+    </div>
+  );
+}
+
+export default App;
